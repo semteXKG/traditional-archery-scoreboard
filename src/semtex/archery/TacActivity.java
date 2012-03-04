01bc97ab21be984234c3885dcee560360a0136e5
Klaus Gradinger <Klaus.Gradinger@gmail.com>
2012-03-04 15:54:55 +0100

    initial import

---
+++ src/semtex/archery/TacActivity.java
@@ -0,0 +1,13 @@
+package semtex.archery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TacActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
