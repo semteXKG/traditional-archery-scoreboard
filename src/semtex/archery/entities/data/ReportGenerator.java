35d885444c7f812558e73585d5877fc9f956dbbc
Klaus Gradinger <Klaus.Gradinger@gmail.com>
2012-04-02 23:05:48 +0200

    added new jar files and implemented a better algorithm for the calculation of scoring cards

--- src/semtex/archery/entities/data/ReportGenerator.java
+++ src/semtex/archery/entities/data/ReportGenerator.java
@@ -11,9 +11,13 @@ package semtex.archery.entities.data;
 import java.util.HashMap;
 import java.util.Map;
 
-import semtex.archery.entities.data.entities.*;
+import semtex.archery.entities.data.entities.Parcour;
+import semtex.archery.entities.data.entities.Version;
+import semtex.archery.entities.data.entities.Visit;
 import semtex.archery.entities.data.reports.ParcourReportData;
 import android.util.Log;
+
+import com.j256.ormlite.dao.GenericRawResults;
 
 
 /**
@@ -45,21 +49,32 @@ public class ReportGenerator {
 
     reportData.setParcourName(parcour.getName());
     reportData.setParcourRevisionDate(version.getCreated());
-
+    Log.i(TAG, "STARTING NEW");
+    final GenericRawResults<String[]> queryRaw =
+        daoHelper.getParcourDao().queryRaw(
+            "SELECT u.userName, target.target_number, target_hit.points FROM visit "
+                + "LEFT JOIN version ON visit.version_id = version.id "
+                + "LEFT JOIN target ON target.version= version.id "
+                + "LEFT JOIN target_hit ON target_hit.target = target.id "
+                + "LEFT JOIN user_visit uv ON target_hit.user = uv.id " + "LEFT JOIN user u ON uv.user_id = u.id "
+                + "WHERE visit.id=" + visit.getId() + " ORDER BY target.target_number");
+    // new DataType[] { DataType.STRING, DataType.INTEGER_OBJ, DataType.INTEGER_OBJ
     final Map<Integer, Map<String, Double>> scoringData = reportData.getScoringData();
 
-    for (final Target target : daoHelper.getTargetDao().findTargetsByVersion(version)) {
-      final HashMap<String, Double> targetHitMap = new HashMap<String, Double>();
-      for (final UserVisit uv : visit.getUserVisit()) {
-        final TargetHit th = daoHelper.getTargetHitDao().findTargetHitByUserVisitAndTarget(uv, target);
-        if (th != null && th.getPoints() != null) {
-          targetHitMap.put(uv.getUser().getUserName(), (double)th.getPoints());
-          totalNumbers.put(uv.getUser().getUserName(), safeGet(totalNumbers, uv.getUser().getUserName()) + 1);
-          totalPoints
-              .put(uv.getUser().getUserName(), safeGet(totalPoints, uv.getUser().getUserName()) + th.getPoints());
+    for (final String[] objects : queryRaw) {
+      final String userName = objects[0];
+      final Integer targetNumber = Integer.valueOf(objects[1]);
+      final Double points = objects[2] != null ? Double.valueOf(objects[2]) : null;
+      if (points != null) {
+        Map<String, Double> targetHitMap = scoringData.get(targetNumber);
+        if (targetHitMap == null) {
+          targetHitMap = new HashMap<String, Double>();
+          scoringData.put(targetNumber, targetHitMap);
         }
+        targetHitMap.put(userName, points);
+        totalNumbers.put(userName, safeGet(totalNumbers, userName) + 1);
+        totalPoints.put(userName, (int)(safeGet(totalPoints, userName) + points));
       }
-      scoringData.put(target.getTargetNumber(), targetHitMap);
     }
 
     final Map<String, Double> avgPoints = new HashMap<String, Double>();
