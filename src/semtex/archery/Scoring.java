c97ad2262c7425aea01fb475bb8f2d45971cdd30
Klaus Gradinger <Klaus.Gradinger@gmail.com>
2012-06-03 23:45:19 +0200

    added nonsens comment

--- src/semtex/archery/Scoring.java
+++ src/semtex/archery/Scoring.java
@@ -181,6 +181,7 @@ public class Scoring extends OrmLiteBaseActivity<DatabaseHelper> {
     // reload everything from database!
     fetchSetupData();
     updateUIElements();
+    // bla
     Toast.makeText(getApplicationContext(), "Successfully changed to new version", Toast.LENGTH_LONG).show();
   }
 
